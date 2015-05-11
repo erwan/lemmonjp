551d158a50efe3204deda110e56d8a7c729260ab
Erwan Loisant <eloisant@gmail.com>
2015-05-11 21:45:25 +0200

    Progress on templating and helpers

--- js/app.js
+++ js/app.js
@@ -1,31 +1,60 @@
 (function($){
 
-"use strict";
+    "use strict";
 
-var prismic = require('prismic.io');
-var Abyssa = require('abyssa');
-var Router = Abyssa.Router;
-var State = Abyssa.State;
-var templates = require('../dist/templates.js');
+    var Q = require('q');
+    var map = require('lodash/collection/map');
+    var Prismic = require('prismic.io').Prismic;
+    var Helpers = require('./prismic-helpers.js');
+    var Abyssa = require('abyssa');
+    var Router = Abyssa.Router;
+    var State = Abyssa.State;
 
-var PRISMIC_URL = "https://lemmonjp.cdn.prismic.io/api";
+    var post_tmpl = require('../tmpl/post.handlebars');
 
-var index = {
-    enter: function() {
-        console.log("Go to index");
+    var PRISMIC_URL = "https://lemmonjp.cdn.prismic.io/api";
+
+    function linkResolver(link, isBroken) {
+        if (link.type == "author") {
+            return "/author/" + link.id + '/' + link.slug;
+        }
+        if (link.type == "category") {
+            return "/category/" + link.uid;
+        }
+        if (link.type == "post") {
+            var date = link.getDate("post.date");
+            return "/" + date.getFullYear() + '/' + date.getMonth() + '/' + date.getDay() + '/' + link.uid;
+        }
+        return "";
     }
-};
 
-var post = {
-    enter: function() {
-        console.log("Go to article");
-    }
-};
+    var index = {
+        enter: function() {
+            Prismic.Api(PRISMIC_URL, function(err, Api) {
+                Helpers.recentPosts(Api, 1).then(function(response) {
+                    console.log("Got resp", response);
+                    var html = map(response.results, function(document) {
+                        return post_tmpl({
+                            title: document.getText('post.title'),
+                            body: document.getStructuredText('post.body').asHtml(linkResolver),
+                            permalink: linkResolver(document)
+                        });
+                    }).join("\n");
+                    $("#content").html(html);
+                });
+            });
+        }
+    };
 
-Router({
-  index: State('/', index),
-    post: State('/:year/:month/:day/:uid', post)
-})
-.init();
+    var post = {
+        enter: function() {
+            console.log("Go to article");
+        }
+    };
+
+    Router({
+        index: State('/', index),
+        post: State('/:year/:month/:day/:uid', post)
+    }).init();
 
 })(jQuery);
